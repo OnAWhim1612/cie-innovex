d50773f24196ad1a2e25705803ecc983ae812d4a
Rishika Das <131580007+OnAWhim1612@users.noreply.github.com>
2023-09-15 09:48:22 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,205 @@
+import pygame as pg
+import sys
+
+import pygame.display
+from sprites import *
+from config import *
+import sys
+
+class game:
+    def __init__(self):
+        pg.init()
+        self.screen = pg.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+        pg.display.set_caption('India to the Moon')
+        self.clock = pg.time.Clock()
+        self.font = pg.font.Font('PressStart2P-Regular.ttf', 32)
+        self.running = True
+        self.intro_background = pg.image.load('img/bg.jpg')
+        self.go = pg.image.load("img/go.jpg")
+        self.timer = 1500
+        self.tfont = pg.font.Font('Roboto-Regular.ttf', 20)
+        self.q_count = 0
+        self.los_i = 0
+
+
+    def build_map(self):
+        for i, row in enumerate(tilemap):
+            for j, column in enumerate(row):
+                ground(self, j, i)
+                if column == "B":
+                    block(self,j,i)
+                if column == "P":
+                    self.Player = Player(self, j, i)
+                if column == "T":
+                    thing(self, j, i)
+                if column == "Q":
+                    Quest(self, j, i)
+
+
+
+
+    def new(self):
+        self.playing = True
+
+        self.all_sprites = pg.sprite.LayeredUpdates()
+        self.crati=  pg.sprite.LayeredUpdates()
+        self.obs = pg.sprite.LayeredUpdates()
+        self.hit = pg.sprite.LayeredUpdates()
+        self.thing =  pg.sprite.LayeredUpdates()
+        self.welcome =  pg.sprite.LayeredUpdates()
+        self.q = pg.sprite.LayeredUpdates()
+        self.player = pg.sprite.LayeredUpdates()
+
+        self.build_map()
+
+
+    def events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                self.running = False
+
+
+
+    def update(self):
+        self.all_sprites.update()
+        pygame.display.flip()
+        self.timer -= 0.00001
+        self.timer = int(self.timer)
+
+        if self.timer <= 0:
+            self.timer = 0
+            self.game_over()
+            self.playing = False
+
+        if self.q_count == 10:
+            self.you_win()
+            self.playing = False
+
+
+
+
+
+    def draw(self):
+        self.screen.fill(black)
+        self.all_sprites.draw(self.screen)
+        timer_text = self.tfont.render(f'Time: {self.timer}', True, black)
+        timer_rect = timer_text.get_rect(bottomright=(WIN_WIDTH, WIN_HEIGHT))
+        self.screen.blit(timer_text, timer_rect)
+        q_text = self.tfont.render(f'Found: {self.q_count}/10', True, black)
+        q_rect = q_text.get_rect(bottomleft=(0, WIN_HEIGHT))
+        self.screen.blit(q_text, q_rect)
+        fact_text = self.tfont.render(list_of_stuff[self.los_i], True, black)
+        fact_rect = fact_text.get_rect()
+        fact_rect.bottom = WIN_HEIGHT  # Set the bottom of the rect to the window's height
+        fact_rect.centerx = WIN_WIDTH/2
+
+        self.screen.blit(fact_text, fact_rect)
+
+
+        self.clock.tick(FPS)
+        pg.display.update()
+
+
+
+
+    def main(self):
+        while self.playing:
+            self.events()
+            self.update()
+            self.draw()
+
+
+
+    def game_over(self):
+        text = self.font.render('Game Over', True, white)
+        text_rect = text.get_rect(center=(WIN_WIDTH/2, WIN_HEIGHT/2))
+
+        restart_button = button(10, 50, 350, 50, white, black, 'Play Again', 32)
+        photo_button = button(10, 50, 200, 50, white, black, 'Launch Photobooth', 32)
+
+        for sprite in self.all_sprites:
+            sprite.kill()
+
+        while self.running:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    self.running = False
+            mouse_pos = pg.mouse.get_pos()
+            mouse_pressed = pg.mouse.get_pressed()
+
+            if restart_button.is_pressed(mouse_pos, mouse_pressed):
+                self.new()
+                self.main()
+
+            self.screen.blit(self.go, (0,0))
+            self.screen.blit(text, text_rect)
+            self.screen.blit(restart_button.image, restart_button.rect)
+
+            self.clock.tick(FPS)
+            pg.display.update()
+
+    def you_win(self):
+        text = self.font.render('You Win', True, white)
+        text_rect = text.get_rect(center=(WIN_WIDTH/2, WIN_HEIGHT/2))
+
+
+        for sprite in self.all_sprites:
+            sprite.kill()
+
+        while self.running:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    self.running = False
+            mouse_pos = pg.mouse.get_pos()
+            mouse_pressed = pg.mouse.get_pressed()
+
+
+            self.screen.blit(self.go, (0,0))
+            self.screen.blit(text, text_rect)
+
+            self.clock.tick(FPS)
+            pg.display.update()
+
+
+    def intro_screen(self):
+        intro = True
+        title = self.font.render('India to the Moon', True, white)
+
+        title_rect = title.get_rect(x=10, y=10)
+        play_button = button(10, 50, 200, 50, white, black, 'play', 32)
+
+        while intro:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    intro = False
+                    self.running = False
+            mouse_pos = pg.mouse.get_pos()
+            mouse_pressed = pg.mouse.get_pressed()
+
+            if play_button.is_pressed(mouse_pos, mouse_pressed):
+                    intro = False
+
+            self.screen.blit(self.intro_background, (0,0))
+            self.screen.blit(title, title_rect)
+            self.screen.blit(play_button.image, play_button.rect)
+            self.clock.tick(FPS)
+            pg.display.update()
+
+
+
+
+
+
+
+
+game = game()
+game.intro_screen()
+game.new()
+
+while game.running:
+    game.main()
+    game.game_over()
+
+pygame.quit()
+sys.exit()
---
+++ sprites.py
@@ -0,0 +1,388 @@
+import pygame as pg
+import pygame.key
+from config import *
+import sys
+import math
+import random
+import serial
+
+class Player(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.game = game
+        self._layer = PLAYER_LAYER
+        self.groups = self.game.all_sprites, self.game.player
+        pg.sprite.Sprite.__init__(self, self.groups)
+
+        self.x = x * tile_size
+        self.y = y * tile_size
+        self.width = tile_size
+        self.height = tile_size
+
+        self.joy = 'neu'
+        self.facing = 'down'
+
+        image_load = pg.image.load("img/rover.png")
+
+
+        self.x_change = 0
+        self.y_change = 0
+
+        self.image = pg.Surface([self.width,self.height])
+        self.image.blit(image_load, (0,0))
+        self.image.set_colorkey(white)
+        self.font = pygame.font.Font('Roboto-Regular.ttf', 32)
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
+        self.welcome_timer = 0
+
+        self.ser = serial.Serial('COM3', 9600)
+        self.display_welcome = False
+
+    def update(self):
+        self.move()
+        self.animate()
+        self.collide_thing()
+        self.rect.x += self.x_change
+        self.collide_blocks('x')
+        self.rect.y += self.y_change
+        self.collide_blocks('y')
+
+        self.x_change = 0
+        self.y_change = 0
+        self.update_joystick()
+
+    def move(self):
+        keys = pygame.key.get_pressed()
+        hits = pg.sprite.spritecollide(self, self.game.obs, False)
+        if keys[pygame.K_LEFT]:
+            for sprite in self.game.all_sprites:
+                if not hits:
+                    sprite.rect.x += player_speed
+            self.x_change-= player_speed
+            self.facing = 'left'
+        if keys[pygame.K_RIGHT]:
+            for sprite in self.game.all_sprites:
+                if not hits:
+                    sprite.rect.x -= player_speed
+            self.x_change+= player_speed
+            self.facing = 'right'
+        if keys[pygame.K_UP]:
+            for sprite in self.game.all_sprites:
+                if not hits:
+                    sprite.rect.y += player_speed
+            self.y_change-= player_speed
+            self.facing = 'up'
+        if keys[pygame.K_DOWN]:
+            for sprite in self.game.all_sprites:
+                if not hits:
+                    sprite.rect.y -= player_speed
+            self.y_change+= player_speed
+            self.facing = 'down'
+
+
+
+
+    def collide_blocks(self, direction):
+        if direction == "x":
+            # Check for collisions in the x direction
+            hits = pg.sprite.spritecollide(self, self.game.obs, False)
+            if hits:
+                if self.x_change > 0:
+                    # Adjust the sprite's position when moving right
+                    self.rect.x = hits[0].rect.left - self.rect.width
+                    for sprite in self.game.all_sprites:
+                        sprite.rect.x+= player_speed
+                elif self.x_change < 0:
+                    # Adjust the sprite's position when moving left
+                    self.rect.x = hits[0].rect.right
+                    for sprite in self.game.all_sprites:
+                        sprite.rect.x-= player_speed
+        elif direction == "y":
+            # Check for collisions in the y direction
+            hits = pg.sprite.spritecollide(self, self.game.obs, False)
+            if hits:
+                if self.y_change > 0:
+                    # Adjust the sprite's position when moving down
+                    self.rect.y = hits[0].rect.top - self.rect.height
+                    for sprite in self.game.all_sprites:
+                        sprite.rect.y+= player_speed
+                elif self.y_change < 0:
+                    # Adjust the sprite's position when moving up
+                    self.rect.y = hits[0].rect.bottom
+                    for sprite in self.game.all_sprites:
+                        sprite.rect.y-= player_speed
+
+
+    def collide_thing(self):
+        hits = pg.sprite.spritecollide(self, self.game.thing, False)
+        if hits:
+            self.kill()
+            self.game.playing = False
+
+    def animate(self):
+        if self.facing == 'down':
+            self.image = pg.image.load("img/rover_down.png")
+            self.image.set_colorkey(white)
+        elif self.facing == 'up':
+            self.image = pg.image.load("img/rover_up.png")
+            self.image.set_colorkey(white)
+        elif self.facing == 'left':
+            self.image = pg.image.load("img/rover_left.png")
+            self.image.set_colorkey(white)
+        elif self.facing == 'right':
+            self.image = pg.image.load("img/rover.png")
+            self.image.set_colorkey(white)
+
+    def update_joystick(self):
+        hits = pg.sprite.spritecollide(self, self.game.obs, False)
+        mouse_x, mouse_y = pg.mouse.get_pos()
+        grid_width = WIN_WIDTH // 3
+        grid_height = WIN_HEIGHT // 3
+        try:
+            data = self.ser.readline().decode().strip()
+            parts = data.split(',')
+            x_value = float(parts[0])
+            y_value = float(parts[1])
+        except ValueError:
+            print("joystick error")
+
+        x_value = float(parts[0])
+        y_value = float(parts[1])
+
+        if x_value > 500 and x_value < 525:
+            if y_value >525:
+                for sprite in self.game.all_sprites:
+                    if not hits:
+                        sprite.rect.y -= player_speed
+                self.y_change += player_speed
+                self.facing = 'down'
+            elif y_value < 500:
+                for sprite in self.game.all_sprites:
+                    if not hits:
+                        sprite.rect.y += player_speed
+                self.y_change -= player_speed
+                self.facing = 'up'
+        elif x_value>525:
+            for sprite in self.game.all_sprites:
+                if not hits:
+                    sprite.rect.x -= player_speed
+            self.x_change += player_speed
+            self.facing = 'right'
+        elif x_value<500 or mouse_x < grid_width:
+            for sprite in self.game.all_sprites:
+                if not hits:
+                    sprite.rect.x += player_speed
+            self.x_change -= player_speed
+            self.facing = 'left'
+        # Define joystick sensitivity and deadzone
+        # joystick_sensitivity = 20
+        # joystick_deadzone = 10
+        #
+        # # Check joystick input for x-axis
+        # if abs(x_value - 512) > joystick_deadzone:
+        #     if x_value > 512 + joystick_sensitivity:
+        #         for sprite in self.game.all_sprites:
+        #             if not hits:
+        #                 sprite.rect.x -= player_speed
+        #         self.x_change += player_speed  # Adjust the speed as needed
+        #         self.facing = 'right'
+        #         print('right')
+        #     elif x_value < 512 - joystick_sensitivity:
+        #         for sprite in self.game.all_sprites:
+        #             if not hits:
+        #                 sprite.rect.x += player_speed
+        #         self.x_change -= player_speed  # Adjust the speed as needed
+        #         self.facing = 'left'
+        #         print('left')
+        #
+        # # Check joystick input for y-axis
+        # if x_value > 500 and x_value < 525:
+        #     if y_value > 512 + joystick_sensitivity:
+        #         for sprite in self.game.all_sprites:
+        #             if not hits:
+        #                 sprite.rect.y -= player_speed
+        #         self.y_change += player_speed  # Adjust the speed as needed
+        #         self.facing = 'down'
+        #         print('down')
+        #     elif y_value < 512 - joystick_sensitivity:
+        #         for sprite in self.game.all_sprites:
+        #             if not hits:
+        #                 sprite.rect.y += player_speed
+        #         self.y_change -= player_speed  # Adjust the speed as needed
+        #         self.facing = 'up'
+        #         print('up')
+        #
+
+
+class thing(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.game = game
+        self._layer = THING_LAYER
+        self.groups = self.game.all_sprites, self.game.thing
+        pg.sprite.Sprite.__init__(self, self.groups)
+
+        self.x = x * tile_size
+        self.y = y * tile_size
+        self.width = tile_size
+        self.height = tile_size
+
+        image_load = pg.image.load("img/thing.png")
+
+        self.x_change = 0
+        self.y_change = 0
+
+        self.facing = random.choice(['left', 'right', 'up', 'down'])
+        self.movement_loop = 0
+        self.travel = random.randint(7, 13)
+
+        self.image = pg.Surface([self.width, self.height])
+        self.image.blit(image_load, (0, 0))
+        self.image.set_colorkey(white)
+
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+    def move(self):
+        if self.facing == 'left':
+            self.x_change-= thing_speed
+            self.movement_loop-=1
+            if self.movement_loop<= -self.travel:
+                self.facing='right'
+        if self.facing == 'right':
+            self.x_change+= thing_speed
+            self.movement_loop+=1
+            if self.movement_loop>= self.travel:
+                self.facing='left'
+        if self.facing == 'up':
+            self.y_change+= thing_speed
+            self.movement_loop+=1
+            if self.movement_loop>= self.travel:
+                self.facing='down'
+        if self.facing == 'down':
+            self.y_change-= thing_speed
+            self.movement_loop-=1
+            if self.movement_loop<= -self.travel:
+                self.facing='up'
+
+
+
+    def update(self):
+        self.move()
+
+        self.rect.x += self.x_change
+        self.rect.y += self.y_change
+        self.x_change = 0
+        self.y_change = 0
+
+
+
+
+class block(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.game = game
+        self._layer = BLOCK_LAYER
+        self.groups = self.game.all_sprites, self.game.obs
+        pg.sprite.Sprite.__init__(self, self.groups)
+
+        self.x = x * tile_size
+        self.y = y * tile_size
+        self.width = tile_size
+        self.height = tile_size
+        image_load = pg.image.load("img/rock.png")
+        self.image = pg.Surface([self.width, self.height])
+        self.image.blit(image_load, (0, 0))
+        self.image.set_colorkey(white)
+
+
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+
+class ground(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.game = game
+        self._layer = GROUND_LAYER
+        self.groups = self.game.all_sprites
+        pg.sprite.Sprite.__init__(self, self.groups)
+
+        self.x = x * tile_size
+        self.y = y * tile_size
+        self.width = tile_size
+        self.height = tile_size
+
+        image_load = pg.image.load("img/ground.png")
+        self.image = pg.Surface([self.width, self.height])
+        self.image.blit(image_load, (0, 0))
+
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+class button:
+    def __init__(self, x, y,width, height, fg, bg, content, fontsize):
+        self.font = pygame.font.Font('PressStart2P-Regular.ttf', fontsize)
+        self.content = content
+
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.fg = fg
+        self.bg = bg
+
+        self.image = pygame.Surface((self.width, self.height))
+        self.image.fill(self.bg)
+        self.rect = self.image.get_rect()
+
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+        self.text = self.font.render(self.content, True, self.fg)
+        self.text_rect = self.text.get_rect(center=(self.width/2, self.height/2))
+
+        self.image.blit(self.text, self.text_rect)
+
+    def is_pressed(self, pos, pressed):
+        if self.rect.collidepoint(pos):
+            if pressed[0]:
+                return True
+            return False
+        return False
+
+class Quest(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.game = game
+        self._layer = BLOCK_LAYER
+        self.groups = self.game.all_sprites, self.game.q
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.q = q
+
+        self.x = x * tile_size
+        self.y = y * tile_size
+        self.width = tile_size
+        self.height = tile_size
+        images = random.choice(['img/q1.png', 'img/q2.png', 'img/q3.png'])
+        image_load = pg.image.load(images)
+        self.image = pg.Surface([self.width, self.height])
+        self.image.blit(image_load, (0, 0))
+        self.image.set_colorkey(white)
+
+
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+    def update(self):
+        self.collide()
+
+    def collide(self):
+        hits = pg.sprite.spritecollide(self, self.game.player, False)
+        if hits:
+            self.kill()
+            self.game.q_count += 1
+            self.game.los_i += 1
+
+
